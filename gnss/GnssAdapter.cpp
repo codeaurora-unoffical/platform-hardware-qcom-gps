9dc1ada4d1cb8d1381f8efa029f5ea4dc8e771a1
Mike Cailean <mcailean@codeaurora.org>
2018-05-21 14:33:14 -0700

    Add AGC value for QZSS measurement report
    Add AGC value for QZSS measurement report
    to Android framework - value is the same as GPS
    
    Change-Id: I1e3106d99d124945be7f1bda725a413be11a75e6
    CRs-fixed: 2245786

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -3216,6 +3216,7 @@ GnssAdapter::getAgcInformation(GnssMeasurementsNotification& measurements, int m
             for (size_t i = 0; i < measurements.count; i++) {
                 switch (measurements.measurements[i].svType) {
                 case GNSS_SV_TYPE_GPS:
+                case GNSS_SV_TYPE_QZSS:
                     measurements.measurements[i].agcLevelDb =
                             reports.mRfAndParams.back().mAgcGps;
                     measurements.measurements[i].flags |=
@@ -3243,7 +3244,6 @@ GnssAdapter::getAgcInformation(GnssMeasurementsNotification& measurements, int m
                             GNSS_MEASUREMENTS_DATA_AUTOMATIC_GAIN_CONTROL_BIT;
                     break;
 
-                case GNSS_SV_TYPE_QZSS:
                 case GNSS_SV_TYPE_SBAS:
                 case GNSS_SV_TYPE_UNKNOWN:
                 default:
