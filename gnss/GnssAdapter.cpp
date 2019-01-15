clarencelol/android_device_xiaomi_sdm660-common

Fix KW P1 related issues
Change-Id: Ifc3cb9dcabf2e07439fd596b5ad33b46ea562b3d
CRs-fixed: 2375836

gnss/GnssAdapter.cpp
@@ -1999,8 +1999,10 @@ GnssAdapter::restartSessions()
     }
 
     // get the LocationOptions that has the smallest interval, which should be the active one
-    TrackingOptions smallestIntervalOptions = {}; // size is zero until set for the first time
-    TrackingOptions highestPowerTrackingOptions = {};
+    TrackingOptions smallestIntervalOptions; // size is zero until set for the first time
+    TrackingOptions highestPowerTrackingOptions;
+    memset(&smallestIntervalOptions, 0, sizeof(smallestIntervalOptions));
+    memset(&highestPowerTrackingOptions, 0, sizeof(highestPowerTrackingOptions));
     for (auto it = mTrackingSessions.begin(); it != mTrackingSessions.end(); ++it) {
         // size of zero means we havent set it yet
         if (0 == smallestIntervalOptions.size ||
@@ -3022,8 +3024,8 @@ GnssAdapter::reportPosition(const UlpLocation& ulpLocation,
         loc_nmea_generate_pos(ulpLocation, locationExtended, mLocSystemInfo,
                               generate_nmea, nmeaArraystr);
         stringstream ss;
-        for (auto sentence : nmeaArraystr) {
-            ss << sentence;
+        for (auto itor = nmeaArraystr.begin(); itor != nmeaArraystr.end(); ++itor) {
+            ss << *itor;
         }
         string s = ss.str();
         reportNmea(s.c_str(), s.length());
@@ -3118,8 +3120,8 @@ GnssAdapter::reportSv(GnssSvNotification& svNotify)
         std::vector<std::string> nmeaArraystr;
         loc_nmea_generate_sv(svNotify, nmeaArraystr);
         stringstream ss;
-        for (auto sentence : nmeaArraystr) {
-            ss << sentence;
+        for (auto itor = nmeaArraystr.begin(); itor != nmeaArraystr.end(); ++itor) {
+            ss << *itor;
         }
         string s = ss.str();
         reportNmea(s.c_str(), s.length());

codeaurora-unoffical/platform-hardware-qcom-gps

gnss/GnssAdapter.cpp
@@ -1399,7 +1399,8 @@ GnssAdapter::restartSessions()
     }
 
     // get the LocationOptions that has the smallest interval, which should be the active one
-    LocationOptions smallestIntervalOptions = {}; // size is zero until set for the first time
+    LocationOptions smallestIntervalOptions; // size is zero until set for the first time
+    memset(&smallestIntervalOptions, 0, sizeof(smallestIntervalOptions));
     for (auto it = mTrackingSessions.begin(); it != mTrackingSessions.end(); ++it) {
         if (0 == smallestIntervalOptions.size || //size of zero means we havent set it yet
             it->second.minInterval < smallestIntervalOptions.minInterval) {
@@ -2467,8 +2468,8 @@ GnssAdapter::reportPosition(const UlpLocation& ulpLocation,
         loc_nmea_generate_pos(ulpLocation, locationExtended, mLocSystemInfo,
                               generate_nmea, nmeaArraystr);
         stringstream ss;
-        for (auto sentence : nmeaArraystr) {
-            ss << sentence;
+        for (auto itor = nmeaArraystr.begin(); itor != nmeaArraystr.end(); ++itor) {
+            ss << *itor;
         }
         string s = ss.str();
         reportNmea(s.c_str(), s.length());
@@ -2578,8 +2579,8 @@ GnssAdapter::reportSv(GnssSvNotification& svNotify)
         std::vector<std::string> nmeaArraystr;
         loc_nmea_generate_sv(svNotify, nmeaArraystr);
         stringstream ss;
-        for (auto sentence : nmeaArraystr) {
-            ss << sentence;
+        for (auto itor = nmeaArraystr.begin(); itor != nmeaArraystr.end(); ++itor) {
+            ss << *itor;
         }
         string s = ss.str();
         reportNmea(s.c_str(), s.length());