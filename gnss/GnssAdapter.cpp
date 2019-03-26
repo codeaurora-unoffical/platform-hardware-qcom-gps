clarencelol/android_device_xiaomi_sdm660-common

Remove tracking redundanty and assure min interval
A tracking interval should not be less than 100ms
If tracking interval or power mode is updated to
the same value, then start tracking should not be
sent to the modem again.
Fix some issues with multiplex with powerMode

Change-Id: I91e5d0577569b96179a5337af0fc91641dc126db
CRs-fixed: 2190953

gnss/GnssAdapter.cpp
@@ -2303,6 +2303,9 @@ GnssAdapter::startTrackingCommand(LocationAPI* client, TrackingOptions& options)
                             mTrackingOptions.tbm, TRACKING_TBM_THRESHOLD_MILLIS);
                     mTrackingOptions.powerMode = GNSS_POWER_MODE_M2;
                 }
+                if (mTrackingOptions.minInterval < MIN_TRACKING_INTERVAL) {
+                    mTrackingOptions.minInterval = MIN_TRACKING_INTERVAL;
+                }
                 // Api doesn't support multiple clients for time based tracking, so mutiplex
                 bool reportToClientWithNoWait =
                         mAdapter.startTrackingMultiplex(mClient, mSessionId, mTrackingOptions);
@@ -2361,6 +2364,7 @@ GnssAdapter::startTrackingMultiplex(LocationAPI* client, uint32_t sessionId,
         }
         if (updateOptions) {
             // restart time based tracking with the newly updated options
+
             startTracking(client, sessionId, multiplexedOptions);
             // need to wait for QMI callback
             reportToClientWithNoWait = false;
@@ -2375,6 +2379,10 @@ void
 GnssAdapter::startTracking(LocationAPI* client, uint32_t sessionId,
         const TrackingOptions& trackingOptions)
 {
+    LOC_LOGd("minInterval %u minDistance %u mode %u powermode %u tbm %u",
+         trackingOptions.minInterval, trackingOptions.minDistance,
+         trackingOptions.mode, trackingOptions.powerMode, trackingOptions.tbm);
+
     LocPosMode locPosMode = {};
     convertOptions(locPosMode, trackingOptions);
 
@@ -2486,6 +2494,9 @@ GnssAdapter::updateTrackingOptionsCommand(LocationAPI* client, uint32_t id,
                         LOC_LOGd("TBM (%d) > %d Falling back to M2 power mode",
                                 mTrackingOptions.tbm, TRACKING_TBM_THRESHOLD_MILLIS);
                         mTrackingOptions.powerMode = GNSS_POWER_MODE_M2;
+                    }
+                    if (mTrackingOptions.minInterval < MIN_TRACKING_INTERVAL) {
+                        mTrackingOptions.minInterval = MIN_TRACKING_INTERVAL;
                     }
                     // Api doesn't support multiple clients for time based tracking, so mutiplex
                     bool reportToClientWithNoWait =

codeaurora-unoffical/platform-hardware-qcom-gps

gnss/GnssAdapter.cpp
@@ -1659,7 +1659,7 @@ GnssAdapter::startTrackingCommand(LocationAPI* client, LocationOptions& options)
         LocApiBase& mApi;
         LocationAPI* mClient;
         uint32_t mSessionId;
-        LocationOptions mOptions;
+        mutable LocationOptions mOptions;
         inline MsgStartTracking(GnssAdapter& adapter,
                                LocApiBase& api,
                                LocationAPI* client,
@@ -1679,6 +1679,9 @@ GnssAdapter::startTrackingCommand(LocationAPI* client, LocationOptions& options)
             } else if (0 == mOptions.size) {
                 err = LOCATION_ERROR_INVALID_PARAMETER;
             } else {
+                if (mOptions.minInterval < MIN_TRACKING_INTERVAL) {
+                    mOptions.minInterval = MIN_TRACKING_INTERVAL;
+                }
                 // Api doesn't support multiple clients for time based tracking, so mutiplex
                 bool reportToClientWithNoWait =
                         mAdapter.startTrackingMultiplex(mClient, mSessionId, mOptions);
@@ -1727,6 +1730,10 @@ bool
 GnssAdapter::startTracking(LocationAPI* client, uint32_t sessionId,
         const LocationOptions& options)
 {
+    LOC_LOGd("minInterval %u minDistance %u mode %u ",
+             options.minInterval, options.minDistance,
+             options.mode);
+
     bool reportToClientWithNoWait = true;
 
     LocPosMode locPosMode = {};
@@ -1866,7 +1873,7 @@ GnssAdapter::updateTrackingOptionsCommand(LocationAPI* client, uint32_t id,
         LocApiBase& mApi;
         LocationAPI* mClient;
         uint32_t mSessionId;
-        LocationOptions mOptions;
+        mutable LocationOptions mOptions;
         inline MsgUpdateTracking(GnssAdapter& adapter,
                                 LocApiBase& api,
                                 LocationAPI* client,
@@ -1884,6 +1891,9 @@ GnssAdapter::updateTrackingOptionsCommand(LocationAPI* client, uint32_t id,
                 if (0 == mOptions.size) {
                     err = LOCATION_ERROR_INVALID_PARAMETER;
                 } else {
+                    if (mOptions.minInterval < MIN_TRACKING_INTERVAL) {
+                        mOptions.minInterval = MIN_TRACKING_INTERVAL;
+                    }
                     // Api doesn't support multiple clients for time based tracking, so mutiplex
                     bool reportToClientWithNoWait =
                             mAdapter.updateTrackingMultiplex(mClient, mSessionId, mOptions);